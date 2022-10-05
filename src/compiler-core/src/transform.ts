12e478a80ac23fb2a7bd0b687058fe7b0df9d273
May <maaye7@126.com>
2022-10-05 19:49:16 +0800

    生成三种联合类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -4,12 +4,18 @@ import { TO_DISPLAY_STRING } from "./runtimeHelper";
 export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   traverseNode(root, context);
-  createRootCodegen(root)
+  createRootCodegen(root);
+
   root.helpers = [...context.helpers.keys()];
 }
 
 function createRootCodegen(root: any) {
-  root.codegenNode = root.children[0];
+  const child = root.children[0];
+  if (child.type === NodeTypes.ELEMENT) {
+    root.codegenNode = child.codegenNode;
+  } else {
+    root.codegenNode = root.children[0];
+  }
 }
 
 function createTransformContext(root: any, options: any): any {
@@ -27,9 +33,11 @@ function createTransformContext(root: any, options: any): any {
 
 function traverseNode(node: any, context) {
   const nodeTransforms = context.nodeTransforms;
+  const exitFns: any = [];
   for (let i = 0; i < nodeTransforms.length; i++) {
     const transform = nodeTransforms[i];
-    transform(node);
+    const onExit = transform(node, context);
+    if (onExit) exitFns.push(onExit);
   }
 
   switch (node.type) {
@@ -44,6 +52,11 @@ function traverseNode(node: any, context) {
     default:
       break;
   }
+
+  let i = exitFns.length;
+  while (i--) {
+    exitFns[i]();
+  }
 }
 function traverseChildren(node: any, context: any) {
   const children = node.children;
@@ -52,5 +65,4 @@ function traverseChildren(node: any, context: any) {
     const node = children[i];
     traverseNode(node, context);
   }
-
 }
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -7,3 +7,5 @@ export const hashChanged = (oldValue, newValue) => !Object.is(oldValue, newValue
 export const hasOwn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
 
 export const EMPTY_OBJ = {}
+
+export const isString = (value) => typeof value === "string"
