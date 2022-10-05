7a9e4093c65011ca52ae5a82d6125ca3cbd5aae1
May <maaye7@126.com>
2022-10-05 17:01:30 +0800

    transform的string类型逻辑

---
+++ src/compiler-core/src/codegen.ts
@@ -0,0 +1,32 @@
+export function generate(ast) {
+  const context = createCodegenContext();
+  const { push } = context;
+  push("return ");
+
+  const functionName = "render";
+  const args = ["_ctx", "_cache"];
+  const signature = args.join(", ");
+
+  push(`function ${functionName}(${signature}){`);
+  push("return ");
+  genNode(ast.codegenNode, context);
+  push("}");
+
+  return {
+    code: context.code,
+  };
+}
+
+function createCodegenContext(): any {
+  return {
+    code: "",
+    push(source) {
+      this.code += source;
+    },
+  };
+}
+
+function genNode(node: any, context) {
+  const { push } = context;
+  push(`'${node.content}'`);
+}
---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,38 @@
+export function transform(root, options = {}) {
+  const context = createTransformContext(root, options);
+  traverseNode(root, context);
+  createRootCodegen(root)
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
+}
+
+function createTransformContext(root: any, options: any): any {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+
+  return context;
+}
+
+function traverseNode(node: any, context) {
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  traverseChildren(node, context);
+}
+function traverseChildren(node: any, context: any) {
+  const children = node.children;
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
